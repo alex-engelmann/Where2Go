6c3e14a1d02d16d0d81ab74e9adb8086ef7d4359
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-25 20:54:05 -0700

    adding Jquery

--- assets/javascript/yelp.js
+++ assets/javascript/yelp.js
@@ -2,7 +2,10 @@
 
 var apiKey = "19PWD0Q8SBucz7O5e8TiQLaoKcsZPFA8-tHMDxmJx_gd-nI4gecHyB6s_Cb3Y5A5HQik0uaWr35Z21PSXvJW-TIA1zr82giN7soQckjYi71Cd45Cw-4MEtP72bS_XHYx";
 
-var place = "Sacramento";
+var place = $("#zipcode").val();
+console.log(place);
+
+place = "Sacramento";
 
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 var queryUrl = "https://api.yelp.com/v3/events?location=" + place;
