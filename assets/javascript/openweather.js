494c64ebc5901b3de08f98eb6281f46a8eaa7614
LiamH <liamhearts@gmail.com>
2019-04-25 20:55:32 -0700

    added working api link

---
+++ assets/javascript/openweather.js
@@ -0,0 +1,6 @@
+//api key = 166a433c57516f51dfab1f7edaed8413
+//api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}
+
+var queryURL = "api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=5&APPID=166a433c57516f51dfab1f7edaed8413"
+var lat;
+var lon;
