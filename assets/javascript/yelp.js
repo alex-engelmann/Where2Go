0fc22338d8e51f67c8c5380f4c0a149ca9f1a640
Alex Engelmann <alex.engelmann@gmail.com>
2019-04-27 13:16:12 -0700

    api working

--- assets/javascript/yelp.js
+++ assets/javascript/yelp.js
@@ -6,6 +6,10 @@ var getEvents = function () {
 
     
     console.log(moment().format('l'));
+    var startUnixTime = moment().unix();
+    var endUnixTime = startUnixTime + 604800;
+    
+    console.log("unix time: " + startUnixTime);
 
     //Get location from user
     var place = $("#zipcode").val();
@@ -15,7 +19,13 @@ var getEvents = function () {
     //Set up AJAX for the API call
     var apiKey = "19PWD0Q8SBucz7O5e8TiQLaoKcsZPFA8-tHMDxmJx_gd-nI4gecHyB6s_Cb3Y5A5HQik0uaWr35Z21PSXvJW-TIA1zr82giN7soQckjYi71Cd45Cw-4MEtP72bS_XHYx";
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    var queryUrl = "https://api.yelp.com/v3/events?location=" + place;
+    var queryUrl = "https://api.yelp.com/v3/events?location=" + place 
+    + "&start_date=" + startUnixTime
+    + "&end_date=" + endUnixTime
+    + "&limit=50"
+    + "&sort_on=time_start"
+    
+    ;
 
     //Make the Yelp Fusion API call
     $.ajax({
@@ -31,6 +41,7 @@ var getEvents = function () {
         console.log(response.events["0"].image_url);
         console.log(response.events["0"].name);
         console.log(response.events["0"].description);
+        console.log(response.events["0"].time_start);
 
     });
 
