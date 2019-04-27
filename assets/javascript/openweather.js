590f65723f4428e5111ea8d08694e4af4bbd2e8f
LiamH <liamhearts@gmail.com>
2019-04-27 13:12:05 -0700

    fixed weather-api, added moment.js back to index.html

--- assets/javascript/openweather.js
+++ assets/javascript/openweather.js
@@ -1,7 +1,7 @@
 //api key = 166a433c57516f51dfab1f7edaed8413
 //api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}
 var zipcode;
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=5&APPID=166a433c57516f51dfab1f7edaed8413"
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413"
 
 
 function weather(queryURL) {
@@ -12,7 +12,8 @@ function weather(queryURL) {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
 
-            // console.log(response)
+            console.log(response)
+            console.log("*****************")
 
             // arrays represent days of weather, [0] = temperature, [1] = weather description, [2] = weather icon, [3] = day of the week
             let day1 = [response.list[0].temp.day, response.list[0].weather[0].main, "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png", moment.unix(response.list[0].dt).format("dddd")]
@@ -22,7 +23,7 @@ function weather(queryURL) {
             let day5 = [response.list[4].temp.day, response.list[4].weather[0].main, "http://openweathermap.org/img/w/" + response.list[4].weather[0].icon + ".png", moment.unix(response.list[4].dt).format("dddd")]
             let day6 = [response.list[5].temp.day, response.list[5].weather[0].main, "http://openweathermap.org/img/w/" + response.list[5].weather[0].icon + ".png", moment.unix(response.list[5].dt).format("dddd")]
             let day7 = [response.list[6].temp.day, response.list[6].weather[0].main, "http://openweathermap.org/img/w/" + response.list[6].weather[0].icon + ".png", moment.unix(response.list[6].dt).format("dddd")]
-
+            
             console.log(day1)
             console.log(day2)
             console.log(day3)
@@ -36,7 +37,7 @@ function weather(queryURL) {
 
 $(".btn").on("click", function (e) {
     e.preventDefault();
-    var zipcode = $("#zipcode").val();
+    var zipcode = $("#search").val();
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413&units=imperial"
 
     console.log(queryURL)
