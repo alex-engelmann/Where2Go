5a9d712903ddead913cee590e8b8c7484994909c
LiamH <liamhearts@gmail.com>
2019-04-27 10:20:04 -0700

    nothing

--- assets/javascript/openweather.js
+++ assets/javascript/openweather.js
@@ -1,7 +1,7 @@
 //api key = 166a433c57516f51dfab1f7edaed8413
 //api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}
 var zipcode;
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=5&APPID=166a433c57516f51dfab1f7edaed8413"
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413"
 
 
 
@@ -14,6 +14,7 @@ function weather(queryURL) {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
             console.log(response)
+            response.
         });
 }
 
