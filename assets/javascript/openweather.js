72d80671f081682b55b32d692ccfc47d782d6c24
LiamH <liamhearts@gmail.com>
2019-04-27 10:40:26 -0700

    working api and added moment.js

--- assets/javascript/openweather.js
+++ assets/javascript/openweather.js
@@ -4,8 +4,6 @@ var zipcode;
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413"
 
 
-
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=95628&cnt=5&APPID=166a433c57516f51dfab1f7edaed8413"
 function weather(queryURL) {
     $.ajax({
         url: queryURL,
@@ -14,15 +12,23 @@ function weather(queryURL) {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
             console.log(response)
-            response.
+            console.log(response.list[0].temp.day)
+            console.log(response.list[1].temp.day)
+            console.log(response.list[2].temp.day)
+            console.log(response.list[3].temp.day)
+            console.log(response.list[4].temp.day)
+            console.log(response.list[5].temp.day)
+            console.log(response.list[6].temp.day)
+
+            
         });
 }
 
 $(".btn").on("click", function (e) {
     e.preventDefault();
     var zipcode = $("#zipcode").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413"
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=5&APPID=166a433c57516f51dfab1f7edaed8413"
     console.log(queryURL)
     weather(queryURL)
 })
