931bc690ced0cfb0cfeb9c61a65bcac31a0419c8
LiamH <liamhearts@gmail.com>
2019-04-27 11:41:41 -0700

    now pulls and stores relevant weather data for 7 days

--- assets/javascript/openweather.js
+++ assets/javascript/openweather.js
@@ -3,6 +3,7 @@
 var zipcode;
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=5&APPID=166a433c57516f51dfab1f7edaed8413"
 
+console.log(moment.unix(1556740800).format("dddd"))
 
 function weather(queryURL) {
     $.ajax({
@@ -11,16 +12,26 @@ function weather(queryURL) {
     })
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
-            console.log(response)
-            let day1 = [response.list[0].temp.day, response.list[0].weather[0].main]
-            let day2Temp = response.list[1].temp.day
-            let day3Temp = response.list[2].temp.day
-            let day4Temp = response.list[3].temp.day
-            let day5Temp = response.list[4].temp.day
-            let day6Temp = response.list[5].temp.day
-            let day7Temp = response.list[6].temp.day
 
-            
+            console.log(response)
+
+            // arrays represent days of weather, [0] = temperature, [1] = weather description, [2] = weather icon, [3] = day of the week
+            let day1 = [response.list[0].temp.day, response.list[0].weather[0].main, "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png", moment.unix(response.list[0].dt).format("dddd")]
+            let day2 = [response.list[1].temp.day, response.list[1].weather[0].main, "http://openweathermap.org/img/w/" + response.list[1].weather[0].icon + ".png", moment.unix(response.list[1].dt).format("dddd")]
+            let day3 = [response.list[2].temp.day, response.list[2].weather[0].main, "http://openweathermap.org/img/w/" + response.list[2].weather[0].icon + ".png", moment.unix(response.list[2].dt).format("dddd")]
+            let day4 = [response.list[3].temp.day, response.list[3].weather[0].main, "http://openweathermap.org/img/w/" + response.list[3].weather[0].icon + ".png", moment.unix(response.list[3].dt).format("dddd")]
+            let day5 = [response.list[4].temp.day, response.list[4].weather[0].main, "http://openweathermap.org/img/w/" + response.list[4].weather[0].icon + ".png", moment.unix(response.list[4].dt).format("dddd")]
+            let day6 = [response.list[5].temp.day, response.list[5].weather[0].main, "http://openweathermap.org/img/w/" + response.list[5].weather[0].icon + ".png", moment.unix(response.list[5].dt).format("dddd")]
+            let day7 = [response.list[6].temp.day, response.list[6].weather[0].main, "http://openweathermap.org/img/w/" + response.list[6].weather[0].icon + ".png", moment.unix(response.list[6].dt).format("dddd")]
+
+            console.log(day1)
+            console.log(day2)
+            console.log(day3)
+            console.log(day4)
+            console.log(day5)
+            console.log(day6)
+            console.log(day7)
+
         });
 }
 
