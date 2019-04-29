3bafba089e48b158d15b535f8b2d150cd6e0ea7d
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-29 11:58:11 -0700

    added more to the index, and added sass

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
     $('.collapsible').collapsible();
+    $('.main').hide();
 });
+
+// display on search
+$(document).on("click", ".btn", function () {
+    $('.main').show();
+    $('.slider-container').hide();
+    $('.search-container').hide();
+})
+
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 4000
+})
