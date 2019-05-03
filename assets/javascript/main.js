94da64a9d86d2ad303e3a2b7f04fff4eb2a1ad89
LiamH <liamhearts@gmail.com>
2019-05-02 19:20:54 -0700

    firebase now storing zipcode data

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -21,10 +21,50 @@ M.Slider.init(slider, {
     interval: 4000
 });
 
-$(document).on("click",'#new-search', function () {
+$(document).on("click", '#new-search', function () {
     $('.main').hide();
     $('.section-new-search').hide();
     $('.slider-container').show();
     $('.search-container').show();
 
 });
+
+
+
+
+
+
+
+// **** Start of Firebase ****
+
+
+$(".btn").on("click", function (e) {
+    e.preventDefault();
+
+    var currentZip = parseInt($("#search").val())
+    var newCount;
+
+
+
+
+    database.ref("zipcode").once("value", function (snapshot) {
+
+        if (snapshot.child(currentZip).exists()) {
+
+            newCount = snapshot.child(currentZip).val()
+            newCount = (newCount.count + 1)
+            database.ref("zipcode").child(currentZip).update({
+                count: newCount
+            })
+            return;
+        }
+
+        else {
+            database.ref("zipcode/" + currentZip).set({
+                count: 1
+            })
+        }
+
+    })
+
+})
--- assets/javascript/openweather.js
+++ assets/javascript/openweather.js
@@ -4,11 +4,6 @@ var zipcode;
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413"
 
 
-// function appendThings(info) {
-//     newImg = $("<img src=" + info[2] + ">")
-//     $("#current-date").append(newImg)
-// }
-
 
 
 function weather(queryURL) {
@@ -18,8 +13,6 @@ function weather(queryURL) {
     })
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
-
-            console.log(response)
 
             // arrays represent days of weather, [0] = temperature, [1] = weather description, [2] = weather icon, [3] = day of the week
             let day1 = [response.list[0].temp.day, response.list[0].weather[0].main, "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png", moment.unix(response.list[0].dt).format("dddd")]
@@ -34,46 +27,31 @@ function weather(queryURL) {
             var newImg = $("<img src=" + day1[2] + ">")
             $("#current-date").text(day1[3] + " " + Math.floor(day1[0]) + String.fromCharCode(176))
             $("#current-date").append(newImg)
-            console.log(newImg)
 
             var newImg = $("<img src=" + day2[2] + ">")
             $("#current-date1").text(day2[3] + " " + Math.floor(day2[0]) + String.fromCharCode(176))
             $("#current-date1").append(newImg)
-            console.log(newImg)
 
             var newImg = $("<img src=" + day3[2] + ">")
             $("#current-date2").text(day3[3] + " " + Math.floor(day3[0]) + String.fromCharCode(176))
             $("#current-date2").append(newImg)
-            console.log(newImg)
 
             var newImg = $("<img src=" + day4[2] + ">")
             $("#current-date3").text(day4[3] + " " + Math.floor(day4[0]) + String.fromCharCode(176))
             $("#current-date3").append(newImg)
-            console.log(newImg)
 
             var newImg = $("<img src=" + day5[2] + ">")
             $("#current-date4").text(day5[3] + " " + Math.floor(day5[0]) + String.fromCharCode(176))
             $("#current-date4").append(newImg)
-            console.log(newImg)
 
             var newImg = $("<img src=" + day6[2] + ">")
             $("#current-date5").text(day6[3] + " " + Math.floor(day6[0]) + String.fromCharCode(176))
             $("#current-date5").append(newImg)
-            console.log(newImg)
 
             var newImg = $("<img src=" + day7[2] + ">")
             $("#current-date6").text(day7[3] + " " + Math.floor(day7[0]) + String.fromCharCode(176))
             $("#current-date6").append(newImg)
-            console.log(newImg)
 
-            // console.log(day1)
-            // console.log(day2)
-            // console.log(day3)
-            // console.log(day4)
-            // console.log(day5)
-            // console.log(day6)
-            // console.log(day7)
-            // appendThings(day1)
         });
 }
 
@@ -81,7 +59,5 @@ $(".btn").on("click", function (e) {
     e.preventDefault();
     var zipcode = $("#search").val();
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" + zipcode + "&cnt=7&APPID=166a433c57516f51dfab1f7edaed8413&units=imperial"
-
-    console.log(queryURL)
     weather(queryURL)
 })
