523fd96cba616c544198d219e205bbf801023d19
Jim Tatarakis <jimtatarakis@gmail.com>
2019-05-02 19:31:54 -0700

    fixed reset and added zipcode filter

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,6 +12,24 @@ $(document).on("click", "#search-btn", function () {
     $('.search-container').hide();
 });
 
+// filter searchbox
+(function($) {
+    $.fn.inputFilter = function(inputFilter) {
+      return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+      });
+    };
+   }(jQuery))
+$("#search").inputFilter(function(value) {
+    return /^\d*$/.test(value); });
+
 // Slider
 const slider = document.querySelector('.slider');
 M.Slider.init(slider, {
@@ -21,10 +39,34 @@ M.Slider.init(slider, {
     interval: 4000
 });
 
-$(document).on("click",'#new-search', function () {
-    $('.main').hide();
-    $('.section-new-search').hide();
-    $('.slider-container').show();
-    $('.search-container').show();
+// reset button
+function refreshPage(){
+    window.location.reload();
+}
 
-});
+// firebase start 
+$(".btn").on("click", function (e) {
+
+    e.preventDefault();
+
+    var currentZip = parseInt($("#search").val())
+    var newCount = 1;
+
+    database.ref("zipcode").once("value", function (snapshot) {
+
+        if (snapshot.child(currentZip).exists()) {
+
+            newCount = snapshot.child(currentZip).val()
+            newCount = (newCount.count + 1)
+            database.ref("zipcode").child(currentZip).update({
+                count: newCount
+            })
+            return;
+        }
+        else {
+            console.log("else is running");
+        }
+
+    })
+
+})
